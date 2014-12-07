f1f108517a8cfadd73e7476f3098e1c53ed01bd4
John Gill <swfiua@gmail.com>
2014-12-07 17:21:47 -0400

    Add TagIcon and TagEula.  Remove start and end from EulaSnippet

--- django_eulasees/models.py
+++ django_eulasees/models.py
@@ -21,14 +21,9 @@ class EulaSnippet(models.Model):
     title = models.CharField(max_length=200)
     eula = models.ForeignKey(RawEula)
     text = models.TextField()
-    start = models.PositiveIntegerField(blank=True)
-    end = models.PositiveIntegerField(blank=True)
     def __str__(self):
 
         return self.title
-
-class SnippetData(models.Model):
-    pass
 
 
 class Tag(models.Model):
@@ -49,6 +44,35 @@ class SnippetTag(models.Model):
     def __str__(self):
 
         return "%s/%s" % (str(self.tag), str(self.snippet))
+
+
+class TagIcon(models.Model):
+    """ Associated an Icon with a tag
+
+    Icon is just name of a file in static/img
+    """
+    tag = models.ForeignKey(Tag)
+    icon = models.CharField(max_length=200)
+
+class TagEula(models.Model):
+    """ Associated another EULA with a tag
+
+    Some EULA's are annoying and reference other documents.
+
+    So, use this to associated another EULA with a tag.
+
+    For example, suppose the foo.com EULA references the foo.com privacy
+    policy.
+
+    Create a new EULA object for the privacy policy.
+
+    Then tag the paragraph that mentions the privacy policy with the foo.com
+    privacy policy tag and create a TagEula object for that tag that
+    references both the privacy policy RawEula object and the privacy policy tag. 
+    """
+    tag = models.ForeignKey(Tag)
+    eula = models.ForeignKey(RawEula)
+    
 
 # admin site stuff
 admin.site.register(RawEula)
